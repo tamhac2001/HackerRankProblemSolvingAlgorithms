cb8c4ed36be3064ea4292c786e60f0596c09b54e
Tam Hắc <tamhac2001@gmail.com>
2020-10-11 08:17:54 +0700

    Complete

---
+++ src/insertionSort2/Solution.java
@@ -0,0 +1,41 @@
+package insertionSort2;
+
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void insertionSort(int arr[]) {
+        int outer,inner;
+        int temp;
+        for(outer = 1;outer<arr.length;outer++) {
+            inner = outer;
+            temp = arr[outer];
+            while(inner>0 && arr[inner-1]>temp) {
+                arr[inner] = arr[inner-1];        
+                inner--;
+                
+            }
+            
+            arr[inner] = temp;
+            display(arr);
+        }
+    }
+    public static void display(int arr[]) {
+        for(int i:arr) {
+            System.out.print(i+" ");
+        }
+        System.out.println();
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int maxSize = sc.nextInt();
+        int arr[] = new int[maxSize];
+        for(int i=0;i<maxSize;i++) {
+            arr[i] = sc.nextInt();
+        }
+        insertionSort(arr);
+
+    }
+
+}
