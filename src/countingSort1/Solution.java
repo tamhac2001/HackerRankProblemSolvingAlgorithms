f623da48653bfc33f29b67be0c63c0ebff325ad8
Tam Hắc <tamhac2001@gmail.com>
2020-10-16 21:03:20 +0700

    Of course TIME IS GOLD

---
+++ src/countingSort1/Solution.java
@@ -0,0 +1,39 @@
+package countingSort1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Solution {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in) );
+		int size = Integer.parseInt(bf.readLine());
+		int arr[] = new int[100];
+		String temp = bf.readLine();
+		int lower = 0;
+		int upper = 50;
+		do {
+			if(upper!=temp.length()) {
+				while(temp.charAt(upper)!=' ') {
+					upper++;	
+				}
+			}
+			
+			String tempSubString = temp.substring(lower, upper);
+			String strArr[] = tempSubString.split(" ");
+			for(int i=0;i<strArr.length;i++) {
+				arr[Integer.parseInt(strArr[i])]++;	
+			}
+			
+			lower=upper+1;
+			upper+=50;
+			if(temp.length()>upper && temp.length()-upper<50) upper=temp.length();
+		}while(upper<=temp.length()) ;
+		for(int i:arr) {
+			System.out.print(i+" ");
+		}
+			
+			
+	}
+}
